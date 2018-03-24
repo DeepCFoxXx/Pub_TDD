bb673677ad72206dcfac3b458eaaaf7c79dd5f63
DeepCFoxXx <learncode23@gmail.com>
2018-03-24 14:34:46 +0000

    customer name passing

---
+++ customer.rb
@@ -0,0 +1,12 @@
+class Customer
+
+  attr_reader :name, :wallet, :age, :drunkenness
+
+  def initialize(name, wallet, age, drunkenness)
+    @name = name
+    @wallet = wallet
+    @age = age
+    @drunkenness = drunkenness
+  end
+
+end
---
+++ specs/customer_spec.rb
@@ -0,0 +1,16 @@
+require("minitest/autorun")
+require_relative("../customer.rb")
+require_relative("../drink.rb")
+
+class CustomerTest < MiniTest::Test
+
+  def setup
+    @drink = Drink.new("Beer", 3.0, 5)
+    @customer = Customer.new("Kara", 10.0, 30, 0)
+  end
+
+  def test_customer_has_name
+    assert_equal("Kara", @customer.name)
+  end
+
+end
