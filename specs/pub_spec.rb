b0eb066bc7f53d1fac6781d71e017076bd3c3957
DeepCFoxXx <learncode23@gmail.com>
2018-03-24 16:22:38 +0000

    pub does not serve if too drunk

--- specs/pub_spec.rb
+++ specs/pub_spec.rb
@@ -68,4 +68,99 @@ class PubTest < MiniTest::Test
     assert_equal(100.0, @pub.till())
   end
 
+  def test_pub_doesnt_serve_at_or_above_50_drunkenness
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    assert_equal(85.0, @customer3.wallet())
+    assert_equal(50, @customer3.drunkenness())
+    assert_equal(115.0, @pub.till())
+  end
+
+end
+require("minitest/autorun")
+require_relative("../pub.rb")
+require_relative("../drink.rb")
+require_relative("../customer.rb")
+
+class PubTest < MiniTest::Test
+
+  def setup
+    @pub = Pub.new("The Old Oak", 100.0, @drinks)
+    @drink1 = Drink.new("Beer", 3.0, 5)
+    @drink2 = Drink.new("Vodka", 5.0, 15)
+    @drink3 = Drink.new("Rum", 4.0, 10)
+    @drinks = [@drink1, @drink2]
+    @customer1 = Customer.new("Kara", 10.0, 30, 0)
+    @customer2 = Customer.new("Liea", 15.0, 17, 0)
+    @customer3 = Customer.new("Stacy", 100.0, 25, 0)
+  end
+
+  def test_pub_has_name
+    assert_equal("The Old Oak", @pub.name)
+  end
+
+  def test_pub_has_till
+    assert_equal(100, @pub.till)
+  end
+
+  def test_pub_has_drinks
+    assert_equal(2, @drinks.length)
+  end
+
+  def test_customer_too_young__returns_true
+    assert_equal(true, @pub.customer_too_young?(@customer2))
+  end
+
+  def test_customer_too_young__returns_false
+    assert_equal(false, @pub.customer_too_young?(@customer1))
+  end
+
+  def test_customer_too_drunk__returns_false
+    assert_equal(false, @pub.customer_too_drunk?(@customer1))
+  end
+
+  def test_customer_too_drunk__returns_true
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    assert_equal(true, @pub.customer_too_drunk?(@customer3))
+  end
+
+  def test_pub_can_serve
+    @pub.serve(@customer1, @drink3)
+    assert_equal(6.0, @customer1.wallet)
+    assert_equal(104.0, @pub.till)
+  end
+
+  def test_pub_checks_age__serves_over_18
+    @pub.serve(@customer1, @drink2)
+    assert_equal(5.0, @customer1.wallet())
+    assert_equal(105.0, @pub.till())
+  end
+
+  def test_pub_checks_age__doesnt_serve_underage
+    @pub.serve(@customer2, @drink2)
+    assert_equal(15.0, @customer2.wallet())
+    assert_equal(100.0, @pub.till())
+  end
+
+  def test_pub_doesnt_serve_at_or_above_50_drunkenness
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    @pub.serve(@customer3, @drink2)
+    assert_equal(80.0, @customer3.wallet())
+    assert_equal(60, @customer3.drunkenness())
+    assert_equal(120.0, @pub.till())
+  end
+
 end
